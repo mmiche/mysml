3f4887b38c1e4a36866cd44538c94a0723b0d69d
mmiche <marcel.miche@unibas.ch>
2025-07-06 16:39:14 +0200

    Enable single risk threshold

--- R/combineDCA.R
+++ R/combineDCA.R
@@ -5,16 +5,15 @@
 #
 combineDCA <- function(plotTblLs=NULL) {
     plotTbl1 <- plotTblLs[[1]]
-    # levels(plotTbl1$label)
-    # plotTbl1$label <- forcats::fct_recode(plotTbl1$label, names(plotTblLs)[1] = "Prediction model", "Treat all" = "Treat all", "Treat none" = "Treat none")
+    # 
     levels(plotTbl1$label) <- c(names(plotTblLs)[1], "Treat all", "Treat none")
-    # print(plotTbl1, n=nrow(plotTbl1))
-    # d <- 4
-    for(d in 2:length(plotTblLs)) {
-        levels(plotTblLs[[d]]$label) <- c(names(plotTblLs)[d], "Treat all", "Treat none")
-        idxAdd <- plotTblLs[[d]]$label == names(plotTblLs)[d]
-        plotTbl1 <- dplyr::bind_rows(plotTbl1, plotTblLs[[d]][idxAdd,])
-        # print(plotTbl1, n=nrow(plotTbl1))
+    # 
+    if(length(plotTblLs)>1) {
+        for(d in 2:length(plotTblLs)) {
+            levels(plotTblLs[[d]]$label) <- c(names(plotTblLs)[d], "Treat all", "Treat none")
+            idxAdd <- plotTblLs[[d]]$label == names(plotTblLs)[d]
+            plotTbl1 <- dplyr::bind_rows(plotTbl1, plotTblLs[[d]][idxAdd,])
+        }
     }
     return(plotTbl1)
 }
